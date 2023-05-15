eca2e0542404ff3b4e32b02f2ddd298a631994e1
Wiktor Fratczak <wiktorft19@gmail.com>
2023-05-15 21:16:23 +0200

    refactoring

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,21 +1,54 @@
 package game;
-import javax.swing.JFrame;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Main extends JFrame {
+    private final JPanel cardPanel;
+    private final CardLayout cardLayout;
+
+    public Main() {
+        setTitle("Nurikabe Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+        setSize(800, 600);
+
+        setLocationRelativeTo(null);
+
+//
+//        NurikabeMenu menuPanel = new NurikabeMenu();
+//        menuPanel.setMainInstance(this);
+//
+////        TablescorePanel tablescorePanel = new TablescorePanel();
+//        NurikabeMainWindow gamePanel = new NurikabeMainWindow();
+//
+//
+//        cardPanel.add(menuPanel, "menu");
+//        cardPanel.add(tablescorePanel, "tablescore");
+//        cardPanel.add(gamePanel, "game");
+
+        add(cardPanel);
+    }
+
+    public void showMenuPanel() {
+        cardLayout.show(cardPanel, "menu");
+    }
+
+    public void showTablescorePanel() {
+//        cardLayout.show(tablescorePanel, "tablescore");
+    }
+
+    public void showGamePanel() {
+//        cardLayout.show(gamePanel, "game");
+    }
+
     public static void main(String[] args) {
-//        JFrame frame = new JFrame("Nurikabe Game");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Board nurikabeBoardPanel = new Board();
-        Solver nurikabeSolver = new Solver();
-        Board nurikabeBoardPanel1 = new Board();
-        System.out.println(nurikabeBoardPanel1.nurikabeBoardPanel);
-
-        // Utworzenie instancji klasy MainWindow i dodanie jej do okna
-//        NurikabeMainWindow mainWindow = new NurikabeMainWindow(nurikabeSolver);
-//        frame.add(mainWindow);
-
-        // Ustawienie rozmiaru okna i wyświetlenie go
-//        frame.setSize(800, 800);
-//        frame.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            Main app = new Main();
+            app.setVisible(true);
+        });
     }
 }
+
